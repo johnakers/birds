1240043ffb95eb62c9b0230bb421aab3e785d15a
johnakers <johnakersemail@gmail.com>
2026-01-07 07:11:07 -0800

    add 04b font

--- source/ButtonGroup.hx
+++ source/ButtonGroup.hx
@@ -2,6 +2,7 @@ package;
 
 import flixel.group.FlxGroup;
 import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
 
 class ButtonGroup extends FlxGroup
 {
@@ -10,9 +11,13 @@ class ButtonGroup extends FlxGroup
 		super();
 
 		var btnAddBird = new FlxButton(10, 10, "Add Bird", onAddBird);
+		btnAddBird.label.font = "assets/fonts/04B_03__.TTF";
+		btnAddBird.label.setFormat(null, 10, FlxColor.BLACK);
 		add(btnAddBird);
 
 		var btnAddFeed = new FlxButton(10, 40, "Add Feed", onAddFeed);
+		btnAddFeed.label.font = "assets/fonts/04B_03__.TTF";
+		btnAddFeed.label.setFormat(null, 10, FlxColor.BLACK);
 		add(btnAddFeed);
 	}
 }
